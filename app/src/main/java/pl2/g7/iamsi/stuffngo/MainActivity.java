9f5270f6b332f31cef903180755639a270ac061c
Fernando José Videira <2211877@my.ipleiria.pt>
2022-12-16 11:49:15 +0000

    Base Android Commit

---
+++ app/src/main/java/pl2/g7/iamsi/stuffngo/MainActivity.java
@@ -0,0 +1,14 @@
+package pl2.g7.iamsi.stuffngo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
