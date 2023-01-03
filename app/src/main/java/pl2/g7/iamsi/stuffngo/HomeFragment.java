f0980fe9c7b5e4afa139c35044a48254696bc68a
JorgeLopes2003 <92525620+JorgeLopes2003@users.noreply.github.com>
2023-01-03 13:29:00 +0000

    Bottom Nav Menu Bar

---
+++ app/src/main/java/pl2/g7/iamsi/stuffngo/HomeFragment.java
@@ -0,0 +1,50 @@
+package pl2.g7.iamsi.stuffngo;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class HomeFragment extends Fragment {
+    ListView lvProdutos;
+    ArrayList<Produtos> produtos;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);;
+
+        lvProdutos = view.findViewById(R.id.lvProdutos);
+
+        produtos = SingletonProdutos.getInstance().getProdutos();
+
+        lvProdutos.setAdapter(new ListaProdutosAdapter(getContext(), produtos));
+
+        lvProdutos.setOnItemClickListener(new AdapterView.OnItemClickListener() { //Ao clique vai para a vista de Detalhes
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                Intent intent = new Intent(getContext(), DetalhesProdutosActivity.class);
+                intent.putExtra(DetalhesProdutosActivity.IDPRODUTO, (int) id);
+                startActivity(intent);
+
+            }
+        });
+        return view;
+    }
+}
--- app/src/main/java/pl2/g7/iamsi/stuffngo/MainActivity.java
+++ app/src/main/java/pl2/g7/iamsi/stuffngo/MainActivity.java
@@ -1,21 +1,63 @@
 package pl2.g7.iamsi.stuffngo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import pl2.g7.iamsi.stuffngo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()){
+                case R.id.home :
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.copoes:
+                    replaceFragment(new CopoesFragment());
+                    break;
+                case R.id.qr:
+                    replaceFragment(new QrFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+
+            }
+
+            return true ;
+        });
+
+
+    }
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+    //Era para o botao dos produtos
    public void OnclickListaProdutos(View view){
        Intent intent = new Intent(this, ListaProdutos.class);
        startActivity(intent);
    };
+
 }
